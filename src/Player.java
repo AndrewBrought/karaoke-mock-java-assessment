ceea869f99a5b636554f92531ababf4bb580370f
Andrew Brought <andrewbrought@gmail.com>
2020-06-25 20:38:34 -0500

    finished play list manipulation for playback

--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,7 @@ public abstract class Player implements Playable {
 //    add an abstract Player class that implements the Playable interface (not it's methods, however) and include the following...
 //    an int constant called WORD_CADENCE set to a value of 600
 //    an int constant called INTRO_PAUSE set to a value of 7000
-    static final int WORD_CADENCE = 350;
+    static final int WORD_CADENCE = 285;
     static final int INTRO_PAUSE = 7000;
 
 }
